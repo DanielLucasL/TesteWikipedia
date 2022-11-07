bf366f7e01701f7b36b56e748beae4745a29d5f8
DanielLucasL <72170522+DanielLucasL@users.noreply.github.com>
2022-11-07 15:26:53 -0300

    Update TestaGoogle.java

--- src/test/java/exselenium/TestaGoogle.java
+++ src/test/java/exselenium/TestaGoogle.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TestaGoogle {
+public class TestaWikipedia {
 
 	protected WebDriver driver;
 
